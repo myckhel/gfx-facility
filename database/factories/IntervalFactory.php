3eb1dd945814e60e2a032917559c435b6c49eea1
Myckhel <myckhel1@hotmail.com>
2020-08-11 21:36:37 +0100

    created intervals migration

---
+++ database/factories/IntervalFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Interval;
+use Faker\Generator as Faker;
+
+$factory->define(Interval::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
